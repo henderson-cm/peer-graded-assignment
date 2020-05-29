a129796e0a220f49b4a3fca668c2150bfddb3e8a
henderson-cm <henderson.cm@gmail.com>
2020-05-29 15:08:22 -0400

    updated readme file

---
+++ guessinggame.sh
@@ -0,0 +1,12 @@
+#! usr/bin/env bash
+# File: guessinggame.sh
+
+# First, ask for user guess
+
+
+
+# Determine number of files in directory and compare to guess
+
+
+
+# Ask for new guess or congratulate user

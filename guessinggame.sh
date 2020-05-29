f76a44e399c5b62bac1df42253b74599504cdf06
henderson-cm <henderson.cm@gmail.com>
2020-05-29 16:01:41 -0400

    completed guessing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,12 +1,30 @@
-#! usr/bin/env bash
+#!/usr/bin/env bash
 # File: guessinggame.sh
 
-# First, ask for user guess
+# Determine the number of files in the directory. 
 
+number=$(ls -l | egrep -c '^-')
 
+# Ask for user guess, compare it to the number of files, and prompt user to try 
+# again if their guess is incorrect. 
 
-# Determine number of files in directory and compare to guess
+guess=-1
 
+while [[ $guess -ne $number ]]
+do
+	echo "How many files are in the current directory? Type a guess and press Enter:"
+	read guess
 
+	if [[ $guess -eq $number ]]
+	then
+		echo "You guessed $guess. That is correct. Congratulations!"
+	elif [[ $guess -gt $number ]]  
+	then
+		echo "You guessed $guess. That is too high. Try again!"
+	elif [[ $guess -lt $number ]]
+	then
+		echo "You guessed $guess. That is too low. Try again!"
+	fi
+	let guess=$guess
+done
 
-# Ask for new guess or congratulate user
